797fa27a95093c7845d9a052aec6047fc0355743
liafernando <91317153+liafernando@users.noreply.github.com>
2025-04-10 14:47:55 +0200

    Updated the search function

--- src/main/java/org/example/datastructures/BinarySearch.java
+++ src/main/java/org/example/datastructures/BinarySearch.java
@@ -2,7 +2,7 @@ package org.example.datastructures1;
 
 public class BinarySearch<T extends Comparable<T>> implements Search<T> {
     @Override
-    public void search(DataStructure<T> dataStructure, T target) {
+    public String search(DataStructure<T> dataStructure, T target) {
         int left = 0;
         int right = dataStructure.size() - 1;
 
@@ -11,8 +11,7 @@ public class BinarySearch<T extends Comparable<T>> implements Search<T> {
             T midValue = dataStructure.get(mid);
 
             if (midValue.equals(target)) {
-                System.out.println("Found at index: " + mid);
-                return;
+                return "Found at index: " + mid;
             }
             if (midValue.compareTo(target) < 0) {
                 left = mid + 1;
@@ -20,6 +19,6 @@ public class BinarySearch<T extends Comparable<T>> implements Search<T> {
                 right = mid - 1;
             }
         }
-        System.out.println("Not found");
+        return "Not found";
     }
 }
